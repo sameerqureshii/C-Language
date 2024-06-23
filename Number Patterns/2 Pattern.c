ac68ec65303cc0634a3954f440fcafec7c996445
Sameer Ahmed Qureshi <93817524+sameerqureshii@users.noreply.github.com>
2024-06-22 22:23:25 -0700

    2 Pattern.c

--- Number Patterns/2 Pattern.c
+++ Number Patterns/2 Pattern.c
@@ -1,15 +1,10 @@
 #include<stdio.h>
-int main()
-{
+int main(){
 	int n=5,mid=n/2+1,i,j;
-	for(i=1; i<=n; i++)
-	{
-		for(j=1; j<=n; j++)
-		{
-			if(i==1 || i==n ||
-			 i==mid || 
-			(j==1 && i>=mid) ||
-			(j==n && i<mid))
+	for(i=1; i<=n; i++){
+		for(j=1; j<=n; j++){
+			
+			if(i==1 || i==n || i==mid || (j==1 && i>=mid) ||(j==n && i<mid))
 			printf("* ");
 			else
 			printf("  ");
